673c7ee238b41da904a5e8c0e86162a58bbc9a1d
twell-star <novikov.igor@mail.ru>
2021-02-20 16:57:17 +0300

    Version 0

--- peer2peer_work2.py
+++ peer2peer_work2.py
@@ -1,1 +1,13 @@
 # Задание на оператор 'if'
+
+cities = ['Москва', 'Париж', 'Лондон']
+
+users = [{'name': 'Иван', 'age': 35},
+         {'name': 'Мария', 'age': 22},
+         {'name': 'Соня', 'age': 20}]
+
+tourists = [{'user': users[0], 'city': cities[2]},
+            {'user': users[1], 'city': cities[0]},
+            {'user': users[2], 'city': cities[1]}]
+
+city = input('Введите город: ')
